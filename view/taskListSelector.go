46e0b49c2f34957c5097e7187b6f97f7ab5199fe
Doug Saylor <doug@saylorsolutions.com>
2021-11-19 00:32:47 -0600

    Adding project menu options and refactoring

--- view/taskListSelector.go
+++ view/taskListSelector.go
@@ -1,15 +1,20 @@
 package view
 
 import (
+	"fmt"
+	"regexp"
 	"sync"
 	"time"
 	"todo/data"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 	"github.com/drognisep/fynehelpers/layouthelp"
+	"github.com/pkg/errors"
 )
 
 type TaskListSelector struct {
@@ -95,6 +100,7 @@ func (s *TaskListSelector) RemoveTaskList(key string) {
 			if v == s.activeList {
 				if k == key {
 					s.multiview.Replace(s.defaultView())
+					s.Selected = ""
 				}
 				break
 			}
@@ -125,4 +131,68 @@ func (s *TaskListSelector) ActiveProject() string {
 
 func (s *TaskListSelector) ViewContainer() *fyne.Container {
 	return s.multiview.Container()
+}
+
+var (
+	labelRegex     = regexp.MustCompile(`^[A-Za-z0-9-_]+( [A-Za-z0-9-_]+)*$`)
+	labelValidator = func(model *data.Model) func(string) error {
+		return func(val string) error {
+			if !labelRegex.MatchString(val) {
+				return errors.New("Must be an alphanumeric string with words separated by only 1 space")
+			}
+			if model.HasProjectName(val) {
+				return errors.New("Project name already exists")
+			}
+			return nil
+		}
+	}
+)
+
+func (s *TaskListSelector) ProjectMenu(ctx *UiCtx) *fyne.Menu {
+	newProject := fyne.NewMenuItem("New", func() {
+		project := &data.Project{
+			Tasks: []*data.Task{},
+		}
+		nameEntry := widget.NewEntryWithData(binding.BindString(&project.Name))
+		nameEntry.Validator = labelValidator(s.model)
+		nameItem := widget.NewFormItem("Project Name", nameEntry)
+		d := dialog.NewForm("New Project", "Create", "Cancel", []*widget.FormItem{nameItem}, func(doCreate bool) {
+			if doCreate {
+				s.model.AddProject(project)
+				s.AddTaskList(project.Name, NewTaskList(ctx, &project.Tasks))
+				s.SetSelected(project.Name)
+			}
+		}, ctx.MainWindow)
+		d.Resize(fyne.NewSize(BigFloat, BigFloat))
+		d.Show()
+	})
+	deleteProject := fyne.NewMenuItem("Delete", func() {
+		activeProject := s.ActiveProject()
+		message := fmt.Sprintf("Are you sure you want to delete '%s'?", activeProject)
+		dialog.ShowConfirm("Are you sure?", message, func(confirmed bool) {
+			if confirmed {
+				s.model.RemoveProject(activeProject)
+				s.RemoveTaskList(activeProject)
+			}
+		}, ctx.MainWindow)
+	})
+	renameProject := fyne.NewMenuItem("Rename", func() {
+		activeProject := s.ActiveProject()
+		activeList := s.ActiveList()
+		newName := activeProject
+		newNameEntry := widget.NewEntryWithData(binding.BindString(&newName))
+		newNameEntry.Validator = labelValidator(s.model)
+		newNameItem := widget.NewFormItem("New Name", newNameEntry)
+		d := dialog.NewForm(fmt.Sprintf("Rename '%s'", activeProject), "Rename", "Cancel", []*widget.FormItem{newNameItem}, func(doRename bool) {
+			if doRename {
+				s.model.RenameProject(activeProject, newName)
+				s.RemoveTaskList(activeProject)
+				s.AddTaskList(newName, activeList)
+				s.SetSelected(newName)
+			}
+		}, ctx.MainWindow)
+		d.Resize(fyne.NewSize(BigFloat, BigFloat))
+		d.Show()
+	})
+	return fyne.NewMenu("Project", newProject, deleteProject, renameProject)
 }
--- view/tasklist.go
+++ view/tasklist.go
@@ -5,10 +5,6 @@ import (
 	"todo/data"
 
 	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/data/binding"
-	"fyne.io/fyne/v2/dialog"
-	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -71,27 +67,4 @@ func (l *TaskList) Tasks() []*data.Task {
 	list := make([]*data.Task, len(l.tasks))
 	copy(list, l.tasks)
 	return list
-}
-
-func TaskPanel(ctx *UiCtx, model *data.Model) *fyne.Container {
-	projectSelect := NewTaskListSelector(ctx, model)
-
-	addTaskBtn := widget.NewButtonWithIcon("New Task", theme.ContentAddIcon(), func() {
-		task := data.Task{}
-		nameEntry := widget.NewEntryWithData(binding.BindString(&task.Name))
-		nameEntry.Validator = TaskNameValidator
-		d := dialog.NewForm("Add Task", "Add", "Cancel", []*widget.FormItem{widget.NewFormItem("Name", nameEntry)}, func(confirm bool) {
-			if confirm {
-				projectSelect.ActiveList().Append(&task)
-			}
-		}, ctx.MainWindow)
-		d.Resize(fyne.NewSize(BigFloat, BigFloat))
-		d.Show()
-	})
-	addTaskBtn.Importance = widget.HighImportance
-	saveBtn := widget.NewButtonWithIcon("", theme.DocumentSaveIcon(), func() {
-		ctx.SaveData(model)
-	})
-	content := container.NewBorder(container.NewHBox(widget.NewLabel("Project"), projectSelect, addTaskBtn, saveBtn), nil, nil, nil, projectSelect.ViewContainer())
-	return content
 }
