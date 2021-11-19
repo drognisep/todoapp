5ecf51f3fc29262df26ea57d6e28cb64052c8486
Doug Saylor <doug@saylorsolutions.com>
2021-11-18 22:02:53 -0600

    Refactoring things out of main

--- main.go
+++ main.go
@@ -8,12 +8,7 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/data/binding"
-	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/driver/desktop"
-	"fyne.io/fyne/v2/theme"
-	"fyne.io/fyne/v2/widget"
 	osdialog "github.com/sqweek/dialog"
 )
 
@@ -43,47 +38,12 @@ func main() {
 		Modifier: desktop.ControlModifier,
 	}
 	w.Canvas().AddShortcut(&ctrlS, func(_ fyne.Shortcut) {
-		saveData(ctx, model)
+		ctx.SaveData(model)
 	})
 
-	w.SetContent(taskPanel(ctx, model))
+	w.SetContent(view.TaskPanel(ctx, model))
 	w.SetOnClosed(func() {
-		saveData(ctx, model)
+		ctx.SaveData(model)
 	})
 	w.ShowAndRun()
-}
-
-func taskPanel(ctx *view.UiCtx, model *data.Model) *fyne.Container {
-	projectSelect := view.NewTaskListSelector(ctx, model)
-
-	addTaskBtn := widget.NewButtonWithIcon("New Task", theme.ContentAddIcon(), func() {
-		task := data.Task{}
-		nameEntry := widget.NewEntryWithData(binding.BindString(&task.Name))
-		nameEntry.Validator = view.TaskNameValidator
-		d := dialog.NewForm("Add Task", "Add", "Cancel", []*widget.FormItem{widget.NewFormItem("Name", nameEntry)}, func(confirm bool) {
-			if confirm {
-				projectSelect.ActiveList().Append(&task)
-			}
-		}, ctx.MainWindow)
-		d.Resize(fyne.NewSize(view.BigFloat, view.BigFloat))
-		d.Show()
-	})
-	addTaskBtn.Importance = widget.HighImportance
-	saveBtn := widget.NewButtonWithIcon("", theme.DocumentSaveIcon(), func() {
-		saveData(ctx, model)
-	})
-	content := container.NewBorder(container.NewHBox(widget.NewLabel("Project"), projectSelect, addTaskBtn, saveBtn), nil, nil, nil, projectSelect.ViewContainer())
-	return content
-}
-
-func saveData(ctx *view.UiCtx, model *data.Model) {
-	lbl := widget.NewLabel("Saving...")
-	inf := widget.NewProgressBarInfinite()
-	content := container.NewVBox(lbl, inf)
-	popup := widget.NewModalPopUp(content, ctx.MainWindow.Canvas())
-	popup.Show()
-	defer popup.Hide()
-	if err := data.SaveTaskData(model); err != nil {
-		dialog.ShowError(err, ctx.MainWindow)
-	}
 }
--- view/context.go
+++ view/context.go
@@ -1,10 +1,27 @@
 package view
 
 import (
+	"todo/data"
+
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/widget"
 )
 
 type UiCtx struct {
 	App        fyne.App
 	MainWindow fyne.Window
+}
+
+func (c *UiCtx) SaveData(model *data.Model) {
+	lbl := widget.NewLabel("Saving...")
+	inf := widget.NewProgressBarInfinite()
+	content := container.NewVBox(lbl, inf)
+	popup := widget.NewModalPopUp(content, c.MainWindow.Canvas())
+	popup.Show()
+	defer popup.Hide()
+	if err := data.SaveTaskData(model); err != nil {
+		dialog.ShowError(err, c.MainWindow)
+	}
 }
